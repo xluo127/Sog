d6823705f455ca504f09cbb2c6bd9ee5dbe187b5
Xiaotian Luo <1521338458@qq.com>
2021-09-03 12:03:39 +0800

    copy

--- src/Sog.jl
+++ src/Sog.jl
@@ -1,5 +1,150 @@
 module Sog
 
-# Write your package code here.
+export sog, tmpsog, zo, zo1, zo3, newsog, newsog2, newsog3, newsog4
+"""
+    sog(x)
+
+The function `sog()` takes a `Vector` of any type `x` as its input with any possible length, and will return a `Bool Vector` with the same length as input. Values
+ `true` or `false` in the output depends on whether one element in the `Vector` is the start of a group or not. The sog() will return 
+ 'nothing' if the length of input is 0. As any type can be applied in the input, be careful! To check whether two elements `a` and `b` are treated
+as the same, please call `isequal(a, b)` to see the result. 
+
+When taking `Vector` of `Vectors`, the length of output should the same as the length of every `Vector`, and what `sog()` do is to compare 
+one element with the previous one on the same position for each `Vector`.
+
+Examples:
+
+    sog([1,2,2,2,1,3,3,1,1]) returns: [true, true, false, false, true, true, false, true, false]
+    sog() returns: nothing
+    sog(["a", "a", "a", 'a']) returns: [true, false, false, true]
+    sog([[1, 1, 1, 2, 2], [1.5, 1.5, 20.0, 3.0, 3.0]] returns: [true, false, true, true, false]
+
+"""
+function sog(x=[])                       
+    if x == []
+        return nothing
+    end
+
+    iVector = x
+    len = length(x)
+    #lenv = length(x[1])
+
+    if typeof(iVector[1]) <: Vector{}           #For Vector of Vectors
+        lenv = length(x[1])
+        if lenv == 0
+            return nothing 
+        end    
+
+        if lenv == 1
+            return [true]
+        end
+
+        if lenv >= 2
+            r0 = map(sog, iVector)
+            rInt = Vector{Vector{Int64}}(r0)
+            oVector = Vector{Bool}(replace(!iszero, sum(rInt)))
+
+            return oVector
+        end 
+    end
+    
+    if len == 1
+        return [true]
+    end
+    
+    r = iVector[1:(len-1)] .=== iVector[2:len]
+    r1 = Vector{Bool}(replace(iszero, r))
+    oVector = Vector{Bool}(append!([1], r1))
+
+
+    return oVector
+end
+
+
+
+function zo(re, xi)
+    for j in 2:length(re)
+        re[j] = re[j]==1 ? 1 : !(xi[j]===xi[j-1])
+    end
+    return re
+end
+    
+    
+function tmpsog(x)
+    re = zeros(Bool, length(x[1]))
+    re[1] = 1
+    for i in 1:length(x)
+        re = zo(re, x[i])
+    end
+    return re
+end
+
+function zo1(re, xi)
+    for j in 2:length(re)
+        @inbounds re[j] = re[j]==1 ? 1 : !(xi[j]===xi[j-1])
+    end
+    return re
+end
+    
+    
+function newsog(x)
+    re = zeros(Bool, length(x[1]))
+    re[1] = 1
+    for i in 1:length(x)
+        re = zo1(re, x[i])
+    end
+    return re
+end
+
+
+
+
+function ini0(x1)
+    len = length(x1)
+    re = zeros(Bool, len)
+    re[1] = 1
+    for j in 2:len
+        @inbounds re[j] = !(x1[j]===x1[j-1])
+    end
+    return re
+end
+
+
+function newsog2(x)
+    re = ini0(x[1])
+    for i in 2:length(x)
+        re = zo1(re, x[i])
+    end
+    return re
+end
+
+function zo3(re, xi)
+    Threads.@threads for j in 2:length(re)
+        @inbounds re[j] = re[j]==1 ? 1 : !(xi[j]===xi[j-1])
+    end
+    return re
+end
+    
+    
+function newsog3(x)
+    re = ini0(x[1])
+    for i in 2:length(x)
+        re = zo3(re, x[i])
+    end
+    return re
+end
+
+function newsog4(x, orders = eachindex(x))
+    if length(eachindex(x)) != length(orders)
+        println("Please specify the order of columns correctly! Number of columns is the length of order.")
+        return nothing
+    end
+    re = ini0(x[orders[1]])
+    for i in orders[2:end]
+        re = zo3(re, x[i])
+    end
+    return re
+end
+
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,26 @@
 using Sog
 using Test
+using Random
 
-@testset "Sog.jl" begin
-    # Write your tests here.
+@testset "MyPkg0.jl" begin
+    @test isequal(sog(), nothing)
+    @test isequal(sog([]), nothing)
+    @test isequal(sog(['o']), Bool[1])
+    @test isequal(sog(["one"]), Bool[1])
+    @test isequal(sog(["one", "two", "one", "one", "two"]), Bool[1, 1, 1, 0, 1])
+    Random.seed!(127)
+    @test isequal(sog(rand(1:10, 20)), Bool[1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1])
+    @test isequal(sog([missing, missing, missing]), Bool[1, 0, 0])
+    @test isequal(sog([missing, 1, missing, missing, 1, 1]), Bool[1, 1, 1, 0, 1, 0])
+    @test isequal(sog(["a", "a", "a", 'a']), Bool[1, 0, 0, 1])
+    @test isequal(sog([1, 1.0, 1.00000]), Bool[1, 0, 0])
+    @test isequal(sog([Inf, Inf32, Inf64]), Bool[1, 0, 0])
+
+    #For Vectors
+    @test isequal(sog([[], []]),nothing)
+    @test isequal(sog([[1, 1, 1, 2, 2], [1.5, 1.5, 20.0, 3.0, 3.0]]), Bool[1, 0, 1, 1, 0])
+    @test isequal(sog([["abc", "ab", "ab"],["ab", "ab", "ab"]]), Bool[1, 1, 0])
+    #@time sog(rand(1:100, 10^6))
+    #xxx = [round.(rand(10^6)), rand(1:10, 10^6), [randstring('a':'e', 2) for _ in 1:10^6]]
+    #@time sog(xxx)
 end
